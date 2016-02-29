822e69e186cf4b9abef38dd0c78f5ac55d609965
Prasad Sodagudi <psodagud@codeaurora.org>
2016-02-29 11:18:15 +0530

    genirq: call cancel_work_sync from irq_set_affinity_notifier
    When ever notification of IRQ affinity changes, call
    cancel_work_sync from irq_set_affinity_notifier to cancel
    all pending works to avoid work list corruption.
    
    Change-Id: I1f093bcc43be8c6696bad29250e4926cbc6c4029
    Signed-off-by: Prasad Sodagudi <psodagud@codeaurora.org>

--- kernel/irq/manage.c
+++ kernel/irq/manage.c
@@ -439,14 +439,12 @@ irq_set_affinity_notifier(unsigned int irq, struct irq_affinity_notify *notify)
 	desc->affinity_notify = notify;
 	raw_spin_unlock_irqrestore(&desc->lock, flags);
 
-	if (old_notify) {
-		if (cancel_work_sync(&old_notify->work)) {
-			/* Pending work had a ref, put that one too */
-			kref_put(&old_notify->kref, old_notify->release);
-		}
-		kref_put(&old_notify->kref, old_notify->release);
-	}
+	if (!notify && old_notify)
+		cancel_work_sync(&old_notify->work);
 
+	if (old_notify)
+		kref_put(&old_notify->kref, old_notify->release);
+		
 	return 0;
 }
 EXPORT_SYMBOL_GPL(irq_set_affinity_notifier);
