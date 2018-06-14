d55b82d5de5142b876912dd05b261504632e231b
Will Deacon <will.deacon@arm.com>
2018-06-14 11:21:34 +0100

    BACKPORT: arm64: Add support for SB barrier and patch in over DSB; ISB sequences
    We currently use a DSB; ISB sequence to inhibit speculation in set_fs().
    Whilst this works for current CPUs, future CPUs may implement a new SB
    barrier instruction which acts as an architected speculation barrier.
    
    On CPUs that support it, patch in an SB; NOP sequence over the DSB; ISB
    sequence and advertise the presence of the new instruction to userspace.
    
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Change-Id: I28e2640aff2734576f64c508cef83ad411e8d3ab

--- arch/arm64/include/asm/cpucaps.h
+++ arch/arm64/include/asm/cpucaps.h
@@ -56,10 +56,11 @@
 #define ARM64_WORKAROUND_1188873		35
 #define ARM64_WORKAROUND_1542418		36
 #define ARM64_WORKAROUND_1542419		37
-#define ARM64_SPECTRE_BHB                    38
-#define ARM64_HAS_CRC32                    39
+#define ARM64_SPECTRE_BHB			38
+#define ARM64_HAS_CRC32				39
+#define ARM64_HAS_SB				40
 
-/* kabi: reserve 38 - 62 for future cpu capabilities */
+/* kabi: reserve 41 - 62 for future cpu capabilities */
 #define ARM64_NCAPS				62
 
 #endif /* __ASM_CPUCAPS_H */
