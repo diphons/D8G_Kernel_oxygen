cf03b5725f1cca1619bd62ad4782bd8f67bb1dba
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2018-10-02 23:45:07 +0200

    UPSTREAM: cpuidle: menu: Do not update last_state_idx in menu_select()
    It is not necessary to update data->last_state_idx in menu_select()
    as it only is used in menu_update() which only runs when
    data->needs_update is set and that is set only when updating
    data->last_state_idx in menu_reflect().
    
    Accordingly, drop the update of data->last_state_idx from
    menu_select() and get rid of the (now redundant) "out" label
    from it.
    
    No intentional behavior changes.
    
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Signed-off-by: Ikjoon Jang <ikjn@chromium.org>
    
    (cherry picked from commit eb40a380bff28f84b6583bba6786b46ef26ef548
    git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git master)
    
    BUG=chromium:1039194
    TEST=Booting-on-Krane
    
    Change-Id: I36dc89486da242df339d0f1604fc2be11b1fd3d2

--- drivers/cpuidle/governors/menu.c
+++ drivers/cpuidle/governors/menu.c
@@ -402,7 +402,7 @@ static int menu_select(struct cpuidle_driver *drv, struct cpuidle_device *dev,
 			    s->target_residency <= ktime_to_us(delta_next))
 				idx = i;
 
-			goto out;
+			return idx;
 		}
 		if (s->exit_latency > latency_req) {
 			/*
@@ -448,10 +448,7 @@ static int menu_select(struct cpuidle_driver *drv, struct cpuidle_device *dev,
 		}
 	}
 
-out:
-	dev->last_state_idx = idx;
-
-	return dev->last_state_idx;
+	return idx;
 }
 
 /**
