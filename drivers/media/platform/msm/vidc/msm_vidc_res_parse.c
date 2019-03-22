5b7bfd6460cf94475619a323d5d1d0bac2ccb2c8
Vikash Garodia <vgarodia@codeaurora.org>
2019-03-22 19:43:29 +0530

    msm: vidc: Remove devfreq module from video
    Bus bandwidth voting can be achieved by directly call
    the bus apis. DevFreq functionality is not utilized nor
    necessary. Also, video bus bandwidth votes are dynamic
    and cannot conform to the OPP table entries.
    
    CRs-Fixed: 2384822
    Change-Id: Ib83f51d7d07efe24a256e0287433e7a28e2ae057
    Signed-off-by: Chinmay Sawarkar <chinmays@codeaurora.org>
    Signed-off-by: Vikash Garodia <vgarodia@codeaurora.org>

--- drivers/media/platform/msm/vidc/msm_vidc_res_parse.c
+++ drivers/media/platform/msm/vidc/msm_vidc_res_parse.c
@@ -436,16 +436,10 @@ static int msm_vidc_populate_bus(struct device *dev,
 		goto err_bus;
 	}
 
-	rc = of_property_read_string(dev->of_node, "qcom,bus-governor",
-			&bus->governor);
-	if (rc) {
-		rc = 0;
-		dprintk(VIDC_DBG,
-				"'qcom,bus-governor' not found, default to performance governor\n");
-		bus->governor = PERF_GOV;
-	}
+	rc = of_property_read_string(dev->of_node, "qcom,mode",
+			&bus->mode);
 
-	if (!strcmp(bus->governor, PERF_GOV))
+	if (!strcmp(bus->mode, PERF_GOV))
 		bus->is_prfm_gov_used = true;
 
 	rc = of_property_read_u32_array(dev->of_node, "qcom,bus-range-kbps",
@@ -463,8 +457,8 @@ static int msm_vidc_populate_bus(struct device *dev,
 
 	buses->count++;
 	bus->dev = dev;
-	dprintk(VIDC_DBG, "Found bus %s [%d->%d] with governor %s\n",
-			bus->name, bus->master, bus->slave, bus->governor);
+	dprintk(VIDC_DBG, "Found bus %s [%d->%d] with mode %s\n",
+			bus->name, bus->master, bus->slave, bus->mode);
 err_bus:
 	return rc;
 }
