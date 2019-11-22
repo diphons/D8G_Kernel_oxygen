82936e3294a9902a7b3af8eca9df8d48989a0331
AngeloGioacchino Del Regno <kholk11@gmail.com>
2019-11-22 11:19:09 +0100

    Android: Implement Sony LowMemoryKiller TNG from 55.0.A.11.25
    This LowMemoryKiller implementation by Sony is cheap and working:
    there's no reason to not use it and write another custom one instead.
    
    This implementation contains little modifications to get it
    building and working on Open Devices.
    
    Signed-off-by: str-br <alexfinhart@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -102,6 +102,8 @@
 #include <linux/devfreq_boost.h>
 #include <misc/d8g_helper.h>
 
+#include <linux/oom_score_notifier.h>
+
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
 #include <linux/uaccess.h>
@@ -2199,6 +2201,11 @@ static __latent_entropy struct task_struct *copy_process(
 
 		init_task_pid(p, PIDTYPE_PID, pid);
 		if (thread_group_leader(p)) {
+#ifdef CONFIG_OOM_SCORE_NOTIFIER
+			retval = oom_score_notify_new(p);
+			if (retval)
+				goto bad_fork_cancel_cgroup;
+#endif
 			init_task_pid(p, PIDTYPE_TGID, pid);
 			init_task_pid(p, PIDTYPE_PGID, task_pgrp(current));
 			init_task_pid(p, PIDTYPE_SID, task_session(current));
