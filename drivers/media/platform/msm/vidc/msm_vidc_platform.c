d475ba1f4c1affb41bb87b3202a79d11aa8ca9ed
AngeloGioacchino Del Regno <kholk11@gmail.com>
2019-11-25 13:33:54 +0100

    media: msm: vidc: platform: Fix sdm845 properties
    Add fw cycles and fw-vpp-cycles for correct calculation of HFR
    and VPP usecases.
    
    Also, fix the power collapse delay and hw response timeout for
    older firmwares.

--- drivers/media/platform/msm/vidc/msm_vidc_platform.c
+++ drivers/media/platform/msm/vidc/msm_vidc_platform.c
@@ -676,11 +676,11 @@ static struct msm_vidc_common_data sdm845_common_data[] = {
 	},
 	{
 		.key = "qcom,power-collapse-delay",
-		.value = 500,
+		.value = 1500,
 	},
 	{
 		.key = "qcom,hw-resp-timeout",
-		.value = 250,
+		.value = 1000,
 	},
 	{
 		.key = "qcom,debug-timeout",
@@ -689,6 +689,14 @@ static struct msm_vidc_common_data sdm845_common_data[] = {
 	{
 		.key = "qcom,dcvs",
 		.value = 1,
+	},
+	{
+		.key = "qcom,fw-cycles",
+		.value = 733003,
+	},
+	{
+		.key = "qcom,fw-vpp-cycles",
+		.value = 225975,
 	},
 };
 
