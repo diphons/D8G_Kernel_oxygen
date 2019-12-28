6815fed792555728f2475ddae7a23df2cc49ea27
Yaroslav Furman <yaro330@gmail.com>
2019-12-28 14:56:34 +0300

    blkdev: switch to SSD mode and entropy gathering
    Optimize for flash storage.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: UtsavisGreat <utsavbalar1231@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -722,7 +722,8 @@ struct request_queue {
 #define QUEUE_FLAG_SCSI_PASSTHROUGH 27	/* queue supports SCSI commands */
 #define QUEUE_FLAG_QUIESCED    28	/* queue has been quiesced */
 
-#define QUEUE_FLAG_DEFAULT	(1 << QUEUE_FLAG_SAME_COMP)
+#define QUEUE_FLAG_DEFAULT	((1 << QUEUE_FLAG_NONROT)	|	\
+				 (1 << QUEUE_FLAG_SAME_COMP))
 
 #define QUEUE_FLAG_MQ_DEFAULT	((1 << QUEUE_FLAG_IO_STAT) |		\
 				 (1 << QUEUE_FLAG_SAME_COMP)	|	\
