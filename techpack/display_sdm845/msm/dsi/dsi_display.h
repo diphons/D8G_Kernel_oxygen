5a195d674dd5ce8506981f2e319e01b48e93bb65
Demon000 <demonsingur@gmail.com>
2020-08-18 01:14:55 +0200

    techpack: display: msm: notify sysfs for fod ui changes
    Change-Id: I5559a9cfe00af58847e089764452a24c39830e20
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- techpack/display_sdm845/msm/dsi/dsi_display.h
+++ techpack/display_sdm845/msm/dsi/dsi_display.h
@@ -283,6 +283,7 @@ struct dsi_display {
 	struct dsi_panel_cmd_set cmd_set;
 
 	bool enabled;
+	atomic_t fod_ui;
 };
 
 int dsi_display_dev_probe(struct platform_device *pdev);
@@ -764,5 +765,7 @@ char *dsi_display_get_cmdline_panel_info(void);
 int dsi_display_hbm_set_disp_param(struct drm_connector *connector,
 				u32 param_type);
 struct dsi_display *get_main_display(void);
+
+void dsi_display_set_fod_ui(struct dsi_display *display, bool status);
 
 #endif /* _DSI_DISPLAY_H_ */
