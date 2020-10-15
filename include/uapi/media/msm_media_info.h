5852ad86c233145b73b705b74289cb7da132aca7
Suprith Malligere Shankaregowda <supgow@codeaurora.org>
2020-10-15 10:38:29 +0530

    msm: vidc: Use older alignments(128x32) for NV12 format
    Use older alignments(128x32) for NV12 format.
    
    Change-Id: Ieac31148a8a9120b36e6d290f1384d6aacc06ec2

--- include/uapi/media/msm_media_info.h
+++ include/uapi/media/msm_media_info.h
@@ -45,10 +45,10 @@ enum color_fmts {
 	 * . . . . . . . . . . . . . . . .  V
 	 * . . . . . . . . . . . . . . . .  --> Buffer size alignment
 	 *
-	 * Y_Stride : Width aligned to 512
-	 * UV_Stride : Width aligned to 512
-	 * Y_Scanlines: Height aligned to 512
-	 * UV_Scanlines: Height/2 aligned to 256
+	 * Y_Stride : Width aligned to 128
+	 * UV_Stride : Width aligned to 128
+	 * Y_Scanlines: Height aligned to 32
+	 * UV_Scanlines: Height/2 aligned to 16
 	 * Total size = align(Y_Stride * Y_Scanlines
 	 *          + UV_Stride * UV_Scanlines, 4096)
 	 */
@@ -115,10 +115,10 @@ enum color_fmts {
 	 * . . . . . . . . . . . . . . . .  V
 	 * . . . . . . . . . . . . . . . .  --> Padding & Buffer size alignment
 	 *
-	 * Y_Stride : Width aligned to 512
-	 * UV_Stride : Width aligned to 512
-	 * Y_Scanlines: Height aligned to 512
-	 * UV_Scanlines: Height/2 aligned to 256
+	 * Y_Stride : Width aligned to 128
+	 * UV_Stride : Width aligned to 128
+	 * Y_Scanlines: Height aligned to 32
+	 * UV_Scanlines: Height/2 aligned to 16
 	 * Total size = align(Y_Stride * Y_Scanlines
 	 *          + UV_Stride * UV_Scanlines, 4096)
 	 */
@@ -827,12 +827,12 @@ static inline unsigned int VENUS_Y_STRIDE(unsigned int color_fmt,
 		goto invalid_input;
 
 	switch (color_fmt) {
-	case COLOR_FMT_NV12:
-	case COLOR_FMT_NV21:
 	case COLOR_FMT_NV12_512:
 		alignment = 512;
 		stride = MSM_MEDIA_ALIGN(width, alignment);
 		break;
+	case COLOR_FMT_NV21:
+	case COLOR_FMT_NV12:
 	case COLOR_FMT_NV12_128:
 	case COLOR_FMT_NV12_UBWC:
 		alignment = 128;
@@ -871,12 +871,12 @@ static inline unsigned int VENUS_UV_STRIDE(unsigned int color_fmt,
 		goto invalid_input;
 
 	switch (color_fmt) {
-	case COLOR_FMT_NV21:
-	case COLOR_FMT_NV12:
 	case COLOR_FMT_NV12_512:
 		alignment = 512;
 		stride = MSM_MEDIA_ALIGN(width, alignment);
 		break;
+	case COLOR_FMT_NV21:
+	case COLOR_FMT_NV12:
 	case COLOR_FMT_NV12_128:
 	case COLOR_FMT_NV12_UBWC:
 		alignment = 128;
@@ -915,11 +915,11 @@ static inline unsigned int VENUS_Y_SCANLINES(unsigned int color_fmt,
 		goto invalid_input;
 
 	switch (color_fmt) {
-	case COLOR_FMT_NV12:
-	case COLOR_FMT_NV21:
 	case COLOR_FMT_NV12_512:
 		alignment = 512;
 		break;
+	case COLOR_FMT_NV21:
+	case COLOR_FMT_NV12:
 	case COLOR_FMT_NV12_128:
 	case COLOR_FMT_NV12_UBWC:
 	case COLOR_FMT_P010:
@@ -953,11 +953,11 @@ static inline unsigned int VENUS_UV_SCANLINES(unsigned int color_fmt,
 		goto invalid_input;
 
 	switch (color_fmt) {
-	case COLOR_FMT_NV21:
-	case COLOR_FMT_NV12:
 	case COLOR_FMT_NV12_512:
 		alignment = 256;
 		break;
+	case COLOR_FMT_NV21:
+	case COLOR_FMT_NV12:
 	case COLOR_FMT_NV12_128:
 	case COLOR_FMT_NV12_BPP10_UBWC:
 	case COLOR_FMT_P010_UBWC:
