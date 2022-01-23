eb01205245362d8478b63bf54fb8b8392371ee57
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2022-01-23 06:58:21 +0600

    fbdev: mdss_fb: Don't create debugfs when !CONFIG_DEBUG_FS
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -1440,7 +1440,9 @@ static int mdss_fb_remove(struct platform_device *pdev)
 		return -EINVAL;
 
 	mdss_fb_unregister_input_handler(mfd);
+#ifdef CONFIG_DEBUG_FS
 	mdss_panel_debugfs_cleanup(mfd->panel_info);
+#endif
 
 	if (mdss_fb_suspend_sub(mfd))
 		pr_err("msm_fb_remove: can't stop the device %d\n",
@@ -2758,8 +2760,10 @@ static int mdss_fb_register(struct msm_fb_data_type *mfd)
 
 	snprintf(panel_name, ARRAY_SIZE(panel_name), "mdss_panel_fb%d",
 		mfd->index);
+#ifdef CONFIG_DEBUG_FS
 	mdss_panel_debugfs_init(panel_info, panel_name);
-	pr_info("FrameBuffer[%d] %dx%d registered successfully!\n", mfd->index,
+	pr_debug("FrameBuffer[%d] %dx%d registered successfully!\n", mfd->index,
+#endif
 					fbi->var.xres, fbi->var.yres);
 
 	return 0;
