5408f683f31df1adefc4c23dfe453157a1fd6362
LuK1337 <priv.luk@gmail.com>
2022-05-06 12:17:09 -0700

    arm64: kernel: Fix offline charging
    Change-Id: I2bb42ca8a92131e79031e2fad0850676a1746377

--- arch/arm64/kernel/setup.c
+++ arch/arm64/kernel/setup.c
@@ -308,6 +308,21 @@ u64 __cpu_logical_map[NR_CPUS] = { [0 ... NR_CPUS-1] = INVALID_HWID };
 
 void __init __weak init_random_pool(void) { }
 
+static int __init androidboot_startupmode(char *p)
+{
+	char *offset_addr;
+
+	if (strcmp(p, "dc_charger"))
+		return 1;
+
+	if ((offset_addr =
+		     strstr(boot_command_line, "androidboot.mode=normal")))
+		memset(offset_addr, ' ', sizeof("androidboot.mode=normal"));
+
+	return 0;
+}
+early_param("androidboot.startupmode", androidboot_startupmode);
+
 void __init setup_arch(char **cmdline_p)
 {
 	init_mm.start_code = (unsigned long) _text;
