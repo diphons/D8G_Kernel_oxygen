30fde398255474f0f310b50be1e3dc9b7838575b
diphons <diphons@gmail.com>
2023-02-17 06:55:24 +0700

    Revert mm/mmap.c: extract __vma_unlink_list() as counterpart for __vma_link_list()

--- mm/mmap.c
+++ mm/mmap.c
@@ -689,8 +689,22 @@ static __always_inline void __vma_unlink_common(struct mm_struct *mm,
 						bool has_prev,
 						struct vm_area_struct *ignore)
 {
+	struct vm_area_struct *next;
+
 	vma_rb_erase_ignore(vma, mm, ignore);
-	__vma_unlink_list(mm, vma);
+	next = vma->vm_next;
+	if (has_prev)
+		prev->vm_next = next;
+	else {
+		prev = vma->vm_prev;
+		if (prev)
+			prev->vm_next = next;
+		else
+			mm->mmap = next;
+	}
+	if (next)
+		next->vm_prev = prev;
+
 	/* Kill the cache */
 	vmacache_invalidate(mm);
 }
