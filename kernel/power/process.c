92f0412b070ef3a862cd9adb573d7ccf44314ea0
diphons <diphons@gmail.com>
2023-03-06 00:23:34 +0700

    kernel/power: faster deepsleep

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -29,7 +29,7 @@
 /*
  * Timeout for stopping processes
  */
-unsigned int __read_mostly freeze_timeout_msecs = 20 * MSEC_PER_SEC;
+unsigned int __read_mostly freeze_timeout_msecs = 2 * MSEC_PER_SEC;
 
 static int try_to_freeze_tasks(bool user_only)
 {
@@ -191,7 +191,7 @@ void thaw_fingerprintd(void)
 
 	read_lock(&tasklist_lock);
 	for_each_process(p) {
-	        if (!strstr(p->comm, "erprint")) {
+	    if (!strstr(p->comm, "erprint")) {
 			__thaw_task(p);
 			break;
 		}
