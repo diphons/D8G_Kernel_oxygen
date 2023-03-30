6d60d0861bf380a66c2bc699b110719d4e3ca745
diphons <diphons@gmail.com>
2023-03-30 11:10:07 +0700

    gpu/msm: adreno_sysfs: Disable KGSL throttling and make it RO
    Squeeze a bit more performance by disabling throttling
    on KGSL.

--- drivers/gpu/msm/adreno_sysfs.c
+++ drivers/gpu/msm/adreno_sysfs.c
@@ -258,7 +258,7 @@ static unsigned int _hwcg_show(struct adreno_device *adreno_dev)
 static int _throttling_store(struct adreno_device *adreno_dev,
 	unsigned int val)
 {
-	return _pwrctrl_store(adreno_dev, val, ADRENO_THROTTLING_CTRL);
+	return 0;
 }
 
 static unsigned int _throttling_show(struct adreno_device *adreno_dev)
@@ -414,6 +414,9 @@ static ssize_t _sysfs_show_bool(struct device *dev,
 #define ADRENO_SYSFS_BOOL(_name) \
 	_ADRENO_SYSFS_ATTR(_name, _sysfs_show_bool, _sysfs_store_bool)
 
+#define ADRENO_SYSFS_RO_BOOL(_name) \
+	_ADRENO_SYSFS_ATTR_RO(_name, _sysfs_show_bool)
+
 #define ADRENO_SYSFS_U32(_name) \
 	_ADRENO_SYSFS_ATTR(_name, _sysfs_show_u32, _sysfs_store_u32)
 
@@ -437,7 +440,7 @@ static ADRENO_SYSFS_BOOL(sptp_pc);
 static ADRENO_SYSFS_BOOL(lm);
 static ADRENO_SYSFS_BOOL(preemption);
 static ADRENO_SYSFS_BOOL(hwcg);
-static ADRENO_SYSFS_BOOL(throttling);
+static ADRENO_SYSFS_RO_BOOL(throttling);
 static ADRENO_SYSFS_BOOL(ifpc);
 static ADRENO_SYSFS_RO_U32(ifpc_count);
 static ADRENO_SYSFS_BOOL(acd);
