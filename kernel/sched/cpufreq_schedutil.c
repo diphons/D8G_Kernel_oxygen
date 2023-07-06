f5ead20fd6bf8185c1f5c3ba1e6596d1c6e167fe
Danda <dandagaharu77@gmail.com>
2023-07-06 20:23:36 +0700

    fix compile error

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -497,7 +497,7 @@ static unsigned long sugov_iowait_apply(struct sugov_cpu *sg_cpu, u64 time,
 #ifdef CONFIG_CPU_IDLE_GOV_TEO
 inline unsigned long sched_cpu_util(int cpu)
 {
-	unsigned long max = arch_scale_cpu_capacity(NULL, cpu);
+	unsigned long max = arch_scale_cpu_capacity(cpu);
 
 	return schedutil_cpu_util(cpu, cpu_util_cfs(cpu_rq(cpu)), max, ENERGY_UTIL, NULL);
 }
