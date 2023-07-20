c93392c3dd52ce7c39250d8f72ae15849ccdb06d
diphons <diphons@gmail.com>
2023-07-20 17:12:56 +0700

    ANDROID: sched: Introducing PELT multiplier
    The new sysctl sched_pelt_multiplier allows a user to set a clock
    multiplier x2 or x4 (x1 being the default). This clock multiplier
    artificially speed-up PELT ramp up/down similarly to a faster half-life.
    Indeed, if we write PELT as a first order filter:
    
      y(t) = G * (1 - exp(t/tau))
    
    Then we can see that multiplying the time by a constant X, is the same
    as
    dividing the time constant tau by X.
    
      y(t) = G * (1 - exp((t*X)/tau))
      y(t) = G * (1 - exp(t/(tau/X)))
    
    Tau being half-life*ln(2), multiplying the PELT time is the same as
    dividing the half-life:
    
      - x1: 32ms half-life
      - x2: 16ms half-life
      - x4: 8ms  half-life
    
    Internally, a new clock is created: rq->clock_task_mult. It sits in the
    clock hierarchy between rq->clock_task and rq->clock_pelt.
    
    Bug: 177593580
    Bug: 237219700
    Change-Id: I67e6ca7994bebea22bf75732ee11d2b10e0d6b7e
    Suggested-by: Morten Rasmussen <morten.rasmussen@arm.com>
    Signed-off-by: Vincent Donnefort <vincent.donnefort@arm.com>
    Signed-off-by: JianMin Liu <jian-min.liu@mediatek.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1159,6 +1159,16 @@ static inline u64 rq_clock_task(struct rq *rq)
 	return rq->clock_task;
 }
 
+DECLARE_PER_CPU(u64, clock_task_mult);
+
+static inline u64 rq_clock_task_mult(struct rq *rq)
+{
+	lockdep_assert_held(&rq->lock);
+	assert_clock_updated(rq);
+
+	return per_cpu(clock_task_mult, rq->cpu);
+}
+
 static inline void rq_clock_skip_update(struct rq *rq)
 {
 	lockdep_assert_held(&rq->lock);
