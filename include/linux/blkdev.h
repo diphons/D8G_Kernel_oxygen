de95433b72de5b1c4bf5a2b7b26752dbc757624c
Rudi Setiyawan <diphons@gmail.com>
2023-10-03 09:12:29 +0700

    block: Disable flag stat and pool for mq sched Signed-off-by: Rudi Setiyawan <diphons@gmail.com>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -727,8 +727,7 @@ struct request_queue {
 #define QUEUE_FLAG_DEFAULT	((1 << QUEUE_FLAG_SAME_COMP)	|	\
 				 (1 << QUEUE_FLAG_ADD_RANDOM))
 
-#define QUEUE_FLAG_MQ_DEFAULT	((1 << QUEUE_FLAG_SAME_COMP)	|	\
-				 (1 << QUEUE_FLAG_POLL))
+#define QUEUE_FLAG_MQ_DEFAULT	(1 << QUEUE_FLAG_SAME_COMP)
 
 void blk_queue_flag_set(unsigned int flag, struct request_queue *q);
 void blk_queue_flag_clear(unsigned int flag, struct request_queue *q);
