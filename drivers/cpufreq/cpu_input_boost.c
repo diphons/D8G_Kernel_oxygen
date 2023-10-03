75d1d58d4fb380078393bba6302fba1f5252711f
Rudi Setiyawan <diphons@gmail.com>
2023-10-03 10:19:17 +0700

    cpu_input_boost: Implement dynamic cpuset for display tasks
    * Migrate display tasks onto big cluster on max boost and move it back on unboost for powersave
    
    Signed-off-by: Rudi Setiyawan <diphons@gmail.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -7,6 +7,7 @@
 
 #include <linux/cpu.h>
 #include <linux/cpufreq.h>
+#include <linux/cpuset.h>
 #include <linux/input.h>
 #include <linux/kthread.h>
 #include <linux/moduleparam.h>
@@ -180,6 +181,8 @@ static void __cpu_input_boost_kick(struct boost_drv *b)
 		return;
 #endif
 
+	do_hp_cpuset();
+
 	set_bit(INPUT_BOOST, &b->state);
 	if (!mod_delayed_work(system_unbound_wq, &b->input_unboost,
 			      msecs_to_jiffies(input_boost_duration))) {
@@ -207,6 +210,8 @@ static void __cpu_input_boost_kick_max(struct boost_drv *b,
 	if (limited || oprofile == 4 || oplus_panel_status != 2)
 		return;
 #endif
+
+	do_hp_cpuset();
 
 	boost_jiffies = msecs_to_jiffies(duration_ms);
 	do {
@@ -240,6 +245,8 @@ static void input_unboost_worker(struct work_struct *work)
 	struct boost_drv *b = container_of(to_delayed_work(work),
 					   typeof(*b), input_unboost);
 
+	do_lp_cpuset();
+
 	clear_bit(INPUT_BOOST, &b->state);
 	wake_up(&b->boost_waitq);
 }
@@ -248,6 +255,8 @@ static void max_unboost_worker(struct work_struct *work)
 {
 	struct boost_drv *b = container_of(to_delayed_work(work),
 					   typeof(*b), max_unboost);
+
+	do_lp_cpuset();
 
 	clear_bit(MAX_BOOST, &b->state);
 	wake_up(&b->boost_waitq);
@@ -381,6 +390,7 @@ free_handle:
 
 static void cpu_input_boost_input_disconnect(struct input_handle *handle)
 {
+	do_lp_cpuset();
 	input_close_device(handle);
 	input_unregister_handle(handle);
 	kfree(handle);
