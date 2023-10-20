86db0c9fb6cc8d0165e958f88c0b8b7795ab8b90
Kazuki H <kazukih0205@gmail.com>
2023-10-21 04:16:06 +0900

    cpuidle: Mark CPUs idle and awake as fast as possible
    Taken from
    https://github.com/kerneltoast/android_kernel_google_gs201/commit/77dd277c62bc41773fe9eb35b40197c219deb29d.
    
    Signed-off-by: Kazuki H <kazukih0205@gmail.com>

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -1298,7 +1298,6 @@ static int psci_enter_idle(struct cpuidle_device *dev, struct lpm_cpu *cpu,
 	if (!idx || is_reserved(dev->cpu)) {
 		if (cpu->bias)
 			biastimer_start(cpu->bias);
-		cpuidle_set_idle_cpu(dev->cpu);
 		stop_critical_timings();
 		cpu_do_idle();
 		start_critical_timings();
@@ -1316,7 +1315,6 @@ static int psci_enter_idle(struct cpuidle_device *dev, struct lpm_cpu *cpu,
 	affinity_level = PSCI_AFFINITY_LEVEL(affinity_level);
 	state_id += power_state + affinity_level + cpu->levels[idx].psci_id;
 
-	cpuidle_set_idle_cpu(dev->cpu);
 	stop_critical_timings();
 
 	success = !arm_cpuidle_suspend(state_id);
