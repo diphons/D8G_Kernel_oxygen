78faf38d18b77c6b50e4af118a5148bfe9d9fa82
Rudi Setiyawan <diphons@gmail.com>
2023-11-03 18:50:09 +0700

    input: fingerprint: fpc_tee: Synch with pelt Signed-off-by: Rudi Setiyawan <diphons@gmail.com>

--- drivers/input/fingerprint/fpc_tee/fpc1020_tee.c
+++ drivers/input/fingerprint/fpc_tee/fpc1020_tee.c
@@ -32,7 +32,6 @@
 #include <linux/atomic.h>
 #include <linux/delay.h>
 #include <linux/gpio.h>
-#include <linux/input.h>
 #include <linux/interrupt.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
@@ -48,8 +47,6 @@
 #ifndef FPC_DRM_INTERFACE_WA
 #include <drm/drm_notifier_mi.h>
 #endif
-
-#define FPC1020_NAME "fpc1020"
 
 #define FPC_GPIO_NO_DEFAULT -1
 #define FPC_GPIO_NO_DEFINED -2
@@ -127,65 +124,6 @@ struct fpc1020_data {
 	bool fb_black;
 	bool wait_finger_down;
 	struct work_struct work;
-#endif
-	struct input_handler input_handler;
-};
-
-static int input_connect(struct input_handler *handler,
-		struct input_dev *dev, const struct input_device_id *id) {
-	int rc;
-	struct input_handle *handle;
-	struct fpc1020_data *fpc1020 =
-		container_of(handler, struct fpc1020_data, input_handler);
-
-	if (!strstr(dev->name, "uinput-fpc"))
-		return -ENODEV;
-
-	handle = kzalloc(sizeof(struct input_handle), GFP_KERNEL);
-	if (!handle)
-		return -ENOMEM;
-
-	handle->dev = dev;
-	handle->handler = handler;
-	handle->name = FPC1020_NAME;
-	handle->private = fpc1020;
-
-	rc = input_register_handle(handle);
-	if (rc)
-		goto err_input_register_handle;
-
-	rc = input_open_device(handle);
-	if (rc)
-		goto err_input_open_device;
-
-	return 0;
-
-err_input_open_device:
-	input_unregister_handle(handle);
-err_input_register_handle:
-	kfree(handle);
-	return rc;
-}
-
-static bool input_filter(struct input_handle *handle, unsigned int type,
-		unsigned int code, int value)
-{
-	return true;
-}
-
-static void input_disconnect(struct input_handle *handle)
-{
-	input_close_device(handle);
-	input_unregister_handle(handle);
-	kfree(handle);
-}
-
-static const struct input_device_id ids[] = {
-	{
-		.flags = INPUT_DEVICE_ID_MATCH_EVBIT,
-		.evbit = { BIT_MASK(EV_KEY) },
-	},
-	{ },
 };
 
 static int reset_gpio_res(struct fpc1020_data *fpc1020);
@@ -1074,7 +1012,7 @@ static int fpc1020_probe(struct platform_device *pdev)
 
 	atomic_set(&fpc1020->wakeup_enabled, 1);
 
-	fpc1020->irqf = IRQF_TRIGGER_RISING | IRQF_ONESHOT | IRQF_LITTLE_AFFINE;
+	fpc1020->irqf = IRQF_TRIGGER_RISING | IRQF_ONESHOT;
 	fpc1020->irq_requested = false;
 	fpc1020->gpios_requested = false;
 	device_init_wakeup(dev, 1);
@@ -1089,17 +1027,6 @@ static int fpc1020_probe(struct platform_device *pdev)
 	fpc1020->ttw_wl = wakeup_source_register(dev, "fpc_ttw_wl");
 	if (!fpc1020->ttw_wl)
 		return -ENOMEM;
-
-        fpc1020->input_handler.filter = input_filter;
-	fpc1020->input_handler.connect = input_connect;
-	fpc1020->input_handler.disconnect = input_disconnect;
-	fpc1020->input_handler.name = FPC1020_NAME;
-	fpc1020->input_handler.id_table = ids;
-	rc = input_register_handler(&fpc1020->input_handler);
-	if (rc) {
-		dev_err(dev, "failed to register key handler\n");
-		goto exit;
-	}
 
 	rc = sysfs_create_group(&dev->kobj, &attribute_group);
 	if (rc) {
@@ -1166,7 +1093,7 @@ MODULE_DEVICE_TABLE(of, fpc1020_of_match);
 
 static struct platform_driver fpc1020_driver = {
 	.driver = {
-		   .name = FPC1020_NAME,
+		   .name = "fpc1020",
 		   .owner = THIS_MODULE,
 		   .of_match_table = fpc1020_of_match,
 		   },
